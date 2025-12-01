c95272beec4960596f041878c8c7cbb12ce4ea2f
Aditya Barve <adityasbarve@gmail.com>
2025-12-01 13:26:28 -0500

    removed border

--- webapp/src/components/AcknowledgementFooter.tsx
+++ webapp/src/components/AcknowledgementFooter.tsx
@@ -5,7 +5,7 @@ export function AcknowledgementFooter() {
 
   return (
     <footer className="mt-8 w-full text-center text-xs uppercase tracking-[0.35em] text-neutral-900/60 p-4">
-      <table className="w-full border">
+      <table className="w-full">
         <tbody>
           <tr>
             <td className="text-center">{t("footer.madeBy")}   |   {t("footer.support")}</td>
